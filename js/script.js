99e80baa4629b040bb27d9efa4ef9b254aa0a53a
Topsylian <topsylian@gmail.com>
2023-05-22 13:46:04 +0300

    hello

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use-strict";
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const show = document.querySelector("p.show");
+const shop = document.querySelector("p.shop");
+
+
+
+/*
+window.addEventListener("load", () => {
+	setTimeout(() => {
+		body.style.opacity = 1;
+	}, 1500);
+})
+*/
