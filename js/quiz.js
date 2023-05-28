e385a8b0105c136d4182a803834d78bc3f217d5e
Topsylian <topsylian@gmail.com>
2023-05-28 15:27:52 +0300

    hmmm

--- js/quiz.js
+++ js/quiz.js
@@ -5,6 +5,14 @@ const dropMenu = document.querySelector(".menu-bar");
 const sticks = document.querySelectorAll("div.stick");
 const crossOne = document.querySelector("#cross-one");
 const crossTwo = document.querySelector("#cross-two");
+const usersName = document.querySelector("#users-name");
+
+/*users's username should show beside the Welcome text*/
+usersName.textContent;
+const userInputName = localStorage.getItem("uname");
+usersName.textContent = userInputName;
+
+/*Hamburger design style and functionalities*/
 
 hamburger.addEventListener("click", () => {
   for (let i = 0; i < sticks.length; i++) {
@@ -12,6 +20,7 @@ hamburger.addEventListener("click", () => {
 			hamburger.classList.add("click");
       sticks[i].style.transition = "800ms ease-in-out";
 			sticks[i].setAttribute("class", "non-stick");
+			dropMenu.style.visibility = "visible";
 			dropMenu.style.height = "100vh";
 			dropMenu.style.width = "90%";
 			crossOne.style.transform = "translate(-8px, -2px) rotate(45deg)";
@@ -20,6 +29,7 @@ hamburger.addEventListener("click", () => {
       sticks[i].style.transition = "800ms ease-in-out";
 			sticks[i].setAttribute("class", "stick");
 			hamburger.classList.remove("click");
+			dropMenu.style.visibility = "hidden";
 			dropMenu.style.height = "0";
 			dropMenu.style.width = "0%";
 			crossOne.style.transform = "translate(50px, -50px) rotate(45deg)";
@@ -27,3 +37,4 @@ hamburger.addEventListener("click", () => {
     }
   }
 });
+
