eacd0964b3effd690ddfd7d9f26bf2877cccef2e
Topsylian <topsylian@gmail.com>
2023-05-28 14:51:45 +0300

    check this

--- js/quiz.js
+++ js/quiz.js
@@ -3,23 +3,27 @@
 const hamburger = document.querySelector(".hamburger-wrapper");
 const dropMenu = document.querySelector(".menu-bar");
 const sticks = document.querySelectorAll("div.stick");
+const crossOne = document.querySelector("#cross-one");
+const crossTwo = document.querySelector("#cross-two");
 
 hamburger.addEventListener("click", () => {
   for (let i = 0; i < sticks.length; i++) {
 		if (sticks[i].getAttribute("class") === "stick") {
 			hamburger.classList.add("click");
       sticks[i].style.transition = "800ms ease-in-out";
-			sticks[i].style.backgroundColor = "green";
 			sticks[i].setAttribute("class", "non-stick");
 			dropMenu.style.height = "100vh";
-			dropMenu.style.width = "70%";
+			dropMenu.style.width = "90%";
+			crossOne.style.transform = "translate(-8px, -2px) rotate(45deg)";
+			crossTwo.style.transform = "translate(-8px, -8px) rotate(-45deg) ";
     } else {
       sticks[i].style.transition = "800ms ease-in-out";
-			sticks[i].style.backgroundColor = "var(--accent-two)";
 			sticks[i].setAttribute("class", "stick");
 			hamburger.classList.remove("click");
 			dropMenu.style.height = "0";
 			dropMenu.style.width = "0%";
+			crossOne.style.transform = "translate(50px, -50px) rotate(45deg)";
+			crossTwo.style.transform = "translate(-50px, 50px) rotate(-45deg) ";
     }
   }
 });
