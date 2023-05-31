307088f89e392d3b7ef32c5818aeb93849900a67
Topsylian <topsylian@gmail.com>
2023-05-31 15:19:09 +0300

    niceeeee

--- js/quiz.js
+++ js/quiz.js
@@ -8,6 +8,8 @@ const crossOne = document.querySelector("#cross-one");
 const crossTwo = document.querySelector("#cross-two");
 const usersName = document.querySelector("#users-name");
 const startBtn = document.querySelector("button");
+const menuProfile = document.querySelector(".menu-bar > div.menu-profile");
+const menuScore = document.querySelector(".menu-bar > div.menu-total-score");
 
 /*users's username should show beside the Welcome text*/
 usersName.textContent;
@@ -24,26 +26,26 @@ hamburger.addEventListener("click", () => {
       sticks[i].setAttribute("class", "non-stick");
       dropMenu.style.display = "flex";
       dropMenu.style.visibility = "visible";
-      dropMenu.style.opacity = "1";
+      menuProfile.style.opacity = "1";
+      menuScore.style.opacity = "1";
       dropMenu.style.height = "100vh";
 			dropMenu.style.width = "90%";
 			dropMenu.style.transform = "translate(0px, 0px)";
       crossOne.style.transform = "translate(-8px, -2px) rotate(45deg)";
-      crossOne.style.backgroundColor = "var(--primary)";
-      crossTwo.style.backgroundColor = "var(--primary)";
       crossTwo.style.transform = "translate(-8px, -8px) rotate(-45deg) ";
-      body.style.background = "var(--accent)";
+      //body.style.background = "var(--accent)";
     } else {
       sticks[i].style.transition = "800ms ease-in-out";
       sticks[i].setAttribute("class", "stick");
       hamburger.classList.remove("click");
       dropMenu.style.visibility = "hidden";
-      dropMenu.style.height = "100vh";
+      dropMenu.style.height = "0vh";
       dropMenu.style.width = "0%";
-      dropMenu.style.opacity = "0";
+      menuProfile.style.opacity = "0";
+      menuScore.style.opacity = "0";
       crossOne.style.transform = "translate(50px, -50px) rotate(45deg)";
       crossTwo.style.transform = "translate(-50px, 50px) rotate(-45deg) ";
-      body.style.background = "var(--primary)";
+      //body.style.background = "var(--primary)";
     }
   }
 });
