2c556f867715ecb34739a2531a37e51ada84bd98
Topsylian <topsylian@gmail.com>
2023-05-31 15:00:40 +0300

    hmmm

--- js/quiz.js
+++ js/quiz.js
@@ -7,7 +7,8 @@ const sticks = document.querySelectorAll("div.stick");
 const crossOne = document.querySelector("#cross-one");
 const crossTwo = document.querySelector("#cross-two");
 const usersName = document.querySelector("#users-name");
-const registerBtn = document.querySelector("button");
+const startBtn = document.querySelector("button");
+
 /*users's username should show beside the Welcome text*/
 usersName.textContent;
 const userInputName = localStorage.getItem("uname");
@@ -17,38 +18,40 @@ usersName.textContent = userInputName;
 
 hamburger.addEventListener("click", () => {
   for (let i = 0; i < sticks.length; i++) {
-		if (sticks[i].getAttribute("class") === "stick") {
-			hamburger.classList.add("click");
+    if (sticks[i].getAttribute("class") === "stick") {
+      hamburger.classList.add("click");
       sticks[i].style.transition = "800ms ease-in-out";
-			sticks[i].setAttribute("class", "non-stick");
-			dropMenu.style.visibility = "visible";
-			dropMenu.style.height = "100vh";
+      sticks[i].setAttribute("class", "non-stick");
+      dropMenu.style.display = "flex";
+      dropMenu.style.visibility = "visible";
+      dropMenu.style.height = "100vh";
 			dropMenu.style.width = "90%";
-			crossOne.style.transform = "translate(-8px, -2px) rotate(45deg)";
-			crossOne.style.backgroundColor = "var(--primary)";
-			crossTwo.style.backgroundColor = "var(--primary)";
-			crossTwo.style.transform = "translate(-8px, -8px) rotate(-45deg) ";
-			body.style.background = "var(--accent)";
+			dropMenu.style.transform = "translate(0px, 0px)";
+      crossOne.style.transform = "translate(-8px, -2px) rotate(45deg)";
+      crossOne.style.backgroundColor = "var(--primary)";
+      crossTwo.style.backgroundColor = "var(--primary)";
+      crossTwo.style.transform = "translate(-8px, -8px) rotate(-45deg) ";
+      body.style.background = "var(--accent)";
     } else {
       sticks[i].style.transition = "800ms ease-in-out";
-			sticks[i].setAttribute("class", "stick");
-			hamburger.classList.remove("click");
-			dropMenu.style.visibility = "hidden";
-			dropMenu.style.height = "0";
+      sticks[i].setAttribute("class", "stick");
+      hamburger.classList.remove("click");
+      dropMenu.style.visibility = "hidden";
+      dropMenu.style.height = "100vh";
 			dropMenu.style.width = "0%";
-			crossOne.style.transform = "translate(50px, -50px) rotate(45deg)";
-			crossTwo.style.transform = "translate(-50px, 50px) rotate(-45deg) ";
-			body.style.background = "var(--primary)";
+      crossOne.style.transform = "translate(50px, -50px) rotate(45deg)";
+      crossTwo.style.transform = "translate(-50px, 50px) rotate(-45deg) ";
+      body.style.background = "var(--primary)";
     }
   }
 });
 
-registerBtn.addEventListener("click", function () {
-	if (registerBtn.getAttribute("class") === "start") {
-    registerBtn.classList.add("active");
-    registerBtn.setAttribute("class", "active");
+startBtn.addEventListener("click", function () {
+  if (startBtn.getAttribute("class") === "start") {
+    startBtn.classList.add("active");
+    startBtn.setAttribute("class", "active");
   } else {
-    registerBtn.classList.remove("active");
-    registerBtn.setAttribute("class", "start");
-	}
+    startBtn.classList.remove("active");
+    startBtn.setAttribute("class", "start");
+  }
 });
